137c107ea3d9ceacb08760631683b4e98ecfacd3
Danil <popovlfybk@yandex.ru>
2026-01-19 18:27:27 +0300

    Add [db initializer]: add automated db initializer/loader

---
+++ src/backend/prepare_db.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+
+import numpy as np
+from PIL import Image
+
+from backend.database import Database
+from backend.embedding_generator import EmbeddingGenerator
+from backend.face_detector import FaceDetector
+from settings import settings
+
+
+def prepare() -> None:
+    db = Database()
+    fd = FaceDetector()
+    eg = EmbeddingGenerator()
+
+    originals = sorted(os.listdir(settings.meme_storage))
+    indexed = []
+
+    for file in originals:
+        print(file)
+
+        img = np.asarray(Image.open(os.path.join(settings.meme_storage, file)))
+
+        face_data = fd.detect(img)
+        if face_data is None:
+            indexed.append(None)
+            continue
+        else:
+            cropped, emb = face_data
+
+        emb, _ = eg.generate_embedding(cropped)
+
+        indexed.append(db.add_vector(emb)[0])
+
+    for original_path, id in zip(originals, indexed):
+        if id is None:
+            continue
+        src = os.path.join(settings.meme_storage, original_path)
+        dst = os.path.join(settings.meme_storage, str(id) + '.jpeg')
+        shutil.copy(src, dst)
+
+    del db
--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
 import uvicorn
 
+from backend.prepare_db import prepare
 from settings import settings
 
 tf.config.set_visible_devices([], "GPU")
 
 if __name__ == '__main__':
+    prepare()
     uvicorn.run(
         settings.uvicorn_app_name,
         port=settings.uvicorn_server_port
